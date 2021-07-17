8c89c9e3185312748c1fe2755c8a11e6f72ea441
Felix <ruslan.rotan.2012@gmail.com>
2021-07-18 00:43:41 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyB25UQ0HmmhS03wVjU0pRki62PFdStLN5k',
+  fbDbUrl: 'https://angular-blog-f-default-rtdb.europe-west1.firebasedatabase.app'
+};
